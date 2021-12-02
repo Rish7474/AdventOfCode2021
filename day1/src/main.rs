49af3c2cc355c15a7bfadffbed2cd452dc175b28
rish <rishmehta2001@icloud.com>
2021-12-01 18:55:43 -0600

    Sonar Sweep Puzzle Solved
    puzzle link: https://adventofcode.com/2021/day/1

---
+++ day1/src/main.rs
@@ -0,0 +1,7 @@
+mod sonor_sweep;
+
+// puzzle link: https://adventofcode.com/2021/day/1
+fn main() {
+    sonor_sweep::calculate_sweep("../input_data.txt");
+    sonor_sweep::calculate_sweep_three("../input_data.txt");
+}
---
+++ day1/src/sonor_sweep.rs
@@ -0,0 +1,47 @@
+use std::env;
+use std::fs;
+
+pub fn calculate_sweep(input_file: &str) {
+    let data = fs::read_to_string(input_file)
+    .expect("File could not be read");
+    let data_itr = data.lines();
+    let data_vec = data_itr.collect::<Vec<&str>>();
+    
+    let mut prev_num : i32 = data_vec[0].parse::<i32>().unwrap();
+    let mut sweep_counter : i32 = 0;
+
+    for i in 1..data_vec.len() {
+        let cur_num = data_vec[i].parse::<i32>().unwrap();
+        if cur_num > prev_num {
+            sweep_counter += 1;
+        }
+        prev_num = cur_num;
+    }
+
+    println!("Total sweep counts: {}", sweep_counter);
+}
+
+pub fn calculate_sweep_three(input_file: &str) {
+    let data = fs::read_to_string(input_file)
+    .expect("File could not be read");
+    let data_itr = data.lines();
+    let data_vec = data_itr.collect::<Vec<&str>>();
+    
+    let mut prev_sum : i32 = 
+        data_vec[0].parse::<i32>().unwrap() +
+        data_vec[1].parse::<i32>().unwrap() +
+        data_vec[2].parse::<i32>().unwrap();
+    let mut sweep_counter : i32 = 0;
+
+    for i in 3..data_vec.len() {
+        let cur_sum = 
+            prev_sum + data_vec[i].parse::<i32>().unwrap()
+            - data_vec[i-3].parse::<i32>().unwrap();
+        if cur_sum > prev_sum {
+            sweep_counter += 1;
+        }
+        prev_sum = cur_sum;
+    }
+
+    println!("Total sweep counts: {}", sweep_counter);   
+}
