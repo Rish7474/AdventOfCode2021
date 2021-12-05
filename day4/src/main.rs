9885f258ee3e2fd9499026b4fc2728079ef4f01e
rish <rishmehta2001@icloud.com>
2021-12-05 09:04:46 -0600

    Giant Squid Bingo Puzzle Solved
    https://adventofcode.com/2021/day/4

---
+++ day4/src/main.rs
@@ -0,0 +1,13 @@
+use std::env;
+use std::fs;
+mod bingo_eval;
+
+// Puzzle Link: https://adventofcode.com/2021/day/4
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 2);
+    let file_content = fs::read_to_string(&args[1]).unwrap();
+    let file_content: Vec<&str> = file_content.lines().filter(|line| !line.is_empty()).collect();
+    println!("Bingo match score of first board: {}", bingo_eval::evaluate_bingo_match(&file_content, 1));
+    println!("Bingo match score of last board: {}", bingo_eval::evaluate_bingo_match(&file_content, -1));
+}
