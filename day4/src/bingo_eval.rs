a0d7919d9606936b16399d4ad7b7dece38198417
rish <rishmehta2001@icloud.com>
2021-12-05 09:07:54 -0600

    Formatted day4 code

--- day4/src/bingo_eval.rs
+++ day4/src/bingo_eval.rs
@@ -1,18 +1,21 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
-const BOARD_SIZE : u32 = 5;
+const BOARD_SIZE: u32 = 5;
 
 pub struct BingoBoard {
     id: i32,
     row_markings: Vec<u32>,
     col_markings: Vec<u32>,
     unmarked_sum: u32,
-    board_map: HashMap<u32, (u32, u32)>
+    board_map: HashMap<u32, (u32, u32)>,
 }
 
 pub fn init_bingo_attributes(bingo_data: &Vec<&str>) -> (Vec<u32>, Vec<BingoBoard>) {
-    let bingo_moves: Vec<u32> = bingo_data[0].split(",").map(|s| s.parse::<u32>().unwrap()).collect();
+    let bingo_moves: Vec<u32> = bingo_data[0]
+        .split(",")
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
     let mut bingo_boards: Vec<BingoBoard> = Vec::new();
     let mut board_id: i32 = 0;
 
@@ -22,24 +25,24 @@ pub fn init_bingo_attributes(bingo_data: &Vec<&str>) -> (Vec<u32>, Vec<BingoBoar
             row_markings: vec![0; BOARD_SIZE.try_into().unwrap()],
             col_markings: vec![0; BOARD_SIZE.try_into().unwrap()],
             unmarked_sum: 0,
-            board_map: HashMap::new()
+            board_map: HashMap::new(),
         };
 
         for r in 0..BOARD_SIZE {
-            let row: Vec<u32> = board_encoding[r as usize].split_whitespace().map(|s| s.parse::<u32>().unwrap()).collect();
+            let row: Vec<u32> = board_encoding[r as usize]
+                .split_whitespace()
+                .map(|s| s.parse::<u32>().unwrap())
+                .collect();
             for c in 0..BOARD_SIZE {
                 bingo_board.unmarked_sum += row[c as usize];
-                bingo_board.board_map.insert(
-                    row[c as usize],
-                    (r, c),
-                );
+                bingo_board.board_map.insert(row[c as usize], (r, c));
             }
         }
         board_id += 1;
         bingo_boards.push(bingo_board);
     }
 
-    return (bingo_moves, bingo_boards)
+    return (bingo_moves, bingo_boards);
 }
 
 pub fn evaluate_bingo_match(bingo_data: &Vec<&str>, ith_win: i32) -> u32 {
@@ -50,20 +53,24 @@ pub fn evaluate_bingo_match(bingo_data: &Vec<&str>, ith_win: i32) -> u32 {
     let mut winning_boards: HashSet<i32> = HashSet::with_capacity(num_boards);
 
     for bingo_move in &bingo_moves {
-        for bingo_board in & mut bingo_boards {
+        for bingo_board in &mut bingo_boards {
             match bingo_board.board_map.get(bingo_move) {
                 Some(&(row, col)) => {
                     bingo_board.unmarked_sum -= bingo_move;
                     bingo_board.row_markings[row as usize] += 1;
                     bingo_board.col_markings[col as usize] += 1;
 
-                    if bingo_board.row_markings[row as usize] == BOARD_SIZE || bingo_board.col_markings[col as usize] == BOARD_SIZE {
+                    if bingo_board.row_markings[row as usize] == BOARD_SIZE
+                        || bingo_board.col_markings[col as usize] == BOARD_SIZE
+                    {
                         winning_boards.insert(bingo_board.id);
-                        if winning_boards.len() as i32 == ith_win || (ith_win == -1 && winning_boards.len() as i32 == num_boards as i32) {
+                        if winning_boards.len() as i32 == ith_win
+                            || (ith_win == -1 && winning_boards.len() as i32 == num_boards as i32)
+                        {
                             return bingo_move * bingo_board.unmarked_sum;
                         }
                     }
-                },
+                }
                 _ => {}
             }
         }
--- day4/src/main.rs
+++ day4/src/main.rs
@@ -7,7 +7,16 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     assert_eq!(args.len(), 2);
     let file_content = fs::read_to_string(&args[1]).unwrap();
-    let file_content: Vec<&str> = file_content.lines().filter(|line| !line.is_empty()).collect();
-    println!("Bingo match score of first board: {}", bingo_eval::evaluate_bingo_match(&file_content, 1));
-    println!("Bingo match score of last board: {}", bingo_eval::evaluate_bingo_match(&file_content, -1));
+    let file_content: Vec<&str> = file_content
+        .lines()
+        .filter(|line| !line.is_empty())
+        .collect();
+    println!(
+        "Bingo match score of first board: {}",
+        bingo_eval::evaluate_bingo_match(&file_content, 1)
+    );
+    println!(
+        "Bingo match score of last board: {}",
+        bingo_eval::evaluate_bingo_match(&file_content, -1)
+    );
 }
